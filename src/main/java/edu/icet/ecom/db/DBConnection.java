609e27313dc2ad0c2c3269339cfb2df1338a223c
Vimarsha <vimarshanethmi2007@gmail.com>
2025-04-29 13:34:14 +0530

    DBConnection

--- src/main/java/edu/icet/ecom/db/DBConnection.java
+++ src/main/java/edu/icet/ecom/db/DBConnection.java
@@ -1,4 +1,25 @@
 package edu.icet.ecom.db;
 
+import lombok.Getter;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private static DBConnection instance;
+    @Getter
+    private Connection connection;
+    private DBConnection() throws SQLException {
+        String URL="jdbc:mysql://localhost:3306/fortium";
+        String userName = "root";
+        String password = "2007";
+        connection = DriverManager.getConnection(URL,userName,password);
+    }
+
+    public DBConnection getInstance() throws SQLException {
+        return instance==null?instance=new DBConnection():instance;
+    }
+
+
 }
