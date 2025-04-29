3c0fc8053e25bcd887df5d26213f4697f85c8e32
Vimarsha <vimarshanethmi2007@gmail.com>
2025-04-29 14:20:41 +0530

    EmployeeController

---
+++ src/main/java/edu/icet/ecom/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package edu.icet.ecom.controller;
+
+import edu.icet.ecom.dto.Employee;
+import edu.icet.ecom.service.custom.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin("")
+@RestController
+public class EmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/get-all")
+    List<Employee> getAll(){
+        return employeeService.getAll();
+    }
+    @DeleteMapping("/delete")
+    void deleteEmployee(Integer id){
+        employeeService.deleteEmployee(id);
+    }
+    @PatchMapping("/update")
+    void updateEmployee(Employee employee,Integer id){
+        employeeService.updateEmployee(employee,id);
+    }
+    @GetMapping
+    List<Employee> searchEmployeeByName(String name){}
+    Employee searchEmployeeByName(Integer id){
+    }
+}
